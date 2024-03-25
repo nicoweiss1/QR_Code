f29b4f5bd53395173dcf2b62b0906844b1a44853
nicoweiss1 <niciweiss@icloud.com>
2024-03-25 21:56:12 +0100

    beqwrf
    hqefg

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+//Konstante(Werte können nicht verändert werden) namens employess
+//in dieser sind 3 Objekte in einem Array die jeweil einen Mitarbeiter darstelle
+//Name ist ein String, stampedIn ein boolean ob der Mitarbeiter eingestempelt ist am anfang false
+//Die Starttime ein Date wann der Mitarbeiter eingestempelt hat, die TotalTime die die gesamte
+//Arbeitszeit darstellt anfang auf 0
 const employees = [
     { name: "Mitarbeiter 1", stampedIn: false, startTime: null, totalTime: 0 },
     { name: "Mitarbeiter 2", stampedIn: false, startTime: null, totalTime: 0 },
     { name: "Mitarbeiter 3", stampedIn: false, startTime: null, totalTime: 0 }
 ];
 
+//funktion updateTime um die Uhrzeit zu aktualisieren. Zuerst neues Date repräsentiert den
+//Momentanen Zeitpunkt der Funktion Date objekt hat viele methoden für die Speicherung von Jahr, Monat, Stunden etc
+//funktion hollt sich also nachher die Stunden, Minuten und Sekunden aus dem Date mit "now" Obejekt oben definiert
+//Diese werden in Strings umgewandelt, mit padStart sagt es wenn der String unter 2 stellen hat fügt es eine 0 hinzu
+//z.B bei 7 = 07, mit getElementbyID zum HTML verknüpft mit stunden minuten sekunden 
 function updateTime() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
@@ -12,6 +22,9 @@ function updateTime() {
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
 
+//stamp funktion für das ein und aus Stempeln des Mitarbeiters. Zuerst neue konstante employee die den
+//Wert des jeweiligen Mitarbeiters nimmt in der Ekigen Klammer wie dann der Parameter dazu heisst
+// wieder das gleiche wie oben schon mit dem Date 
 function stamp(employeeIndex) {
     const employee = employees[employeeIndex];
     const now = new Date();
